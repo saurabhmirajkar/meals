b3133f0768942895e23b11c7e6e2704db12eccdf
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-10-12 16:53:03 +0530

    state managed for favorites section, added snackbar for feedback of favoite button

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,22 +1,30 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meal_detail.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({
-    super.key,
-    required this.title,
-    required this.meals,
-  });
+  const MealsScreen(
+      {super.key,
+      this.title,
+      required this.meals,
+      required this.onToggleFavorite});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectMeal(BuildContext context, Meal meal) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (ctx) => MealtDetailScreen(meal: meal)),
+      MaterialPageRoute(
+        builder: (ctx) => MealtDetailScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
     );
   }
 
@@ -57,9 +65,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
